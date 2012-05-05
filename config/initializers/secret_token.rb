1ab72fb1c7096253bba2716a3aa0a40ee5f436a8
jec1477 <jec1477@gmail.com>
2012-05-05 11:26:56 -0400

    fixed commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Syllabus::Application.config.secret_token = '1624493f799ac74411d4d29d7e7843e4bfbb44bde06d5947adbab280c76c5bf7e11410d3444bfb9f0108128fd0bfbd1214ffd4799d8f44344a2c004f52adf175'
